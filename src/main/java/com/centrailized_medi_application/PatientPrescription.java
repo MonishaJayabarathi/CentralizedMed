2d43f72ac445168bc3929f718ed6ff371cb167fb
antorhasan <antor@ra.cse.buet.ac.bd>
2021-07-02 00:35:54 +0600

    Setup mock test data and Add skeleton class for patient prescription feature

---
+++ src/main/java/com/centrailized_medi_application/PatientPrescription.java
@@ -0,0 +1,23 @@
+package com.centrailized_medi_application;
+
+import java.sql.*;
+
+/* Class representing prescription of a patient */
+public class PatientPrescription {
+
+    /* given patientID, return prescription details from DB
+     * connect to DB
+     * execute sql query
+     * return DB query output */
+    public PatientPrescription(String patientID){
+
+    }
+
+
+    /* print prescription details to patient
+     * format medication */
+    public String getPrescriptionFromDB(ResultSet resultSet){
+        return "";
+    }
+
+}
