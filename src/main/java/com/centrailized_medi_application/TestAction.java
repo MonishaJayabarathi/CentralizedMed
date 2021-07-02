44d934cd807ee623fde94f3beb71b4a842a50007
Monisha J <mn504849@dal.ca>
2021-07-03 00:10:01 +0530

    Added a new class with test to handle doctor  registration which implements Registration(holding common methods that can be overridden)

--- src/main/java/com/centrailized_medi_application/TestAction.java
+++ src/main/java/com/centrailized_medi_application/TestAction.java
@@ -2,6 +2,7 @@ package com.centrailized_medi_application;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class TestAction
 {
@@ -10,6 +11,21 @@ public class TestAction
     }
 
     public TestAction() throws SQLException, IOException, ClassNotFoundException {
+        System.out.println("\n*****Centralized Medi-Application*****\n");
+        System.out.println("-----------Main Menu-------------");
+        System.out.println(" 1.Register as a Patient\n 2.Login as a Patient\n 3.Register as a Doctor\n 4.Login as a Doctor");
+        System.out.println("----------------------------------");
+        System.out.println(" Enter from above options to proceed:");
+
+        //Receive input and instantiate corresponding class
+        Scanner sc = new Scanner(System.in);
+        if(sc.nextInt() == 3) {
+            DoctorRegistration doc = new DoctorRegistration();
+            doc.run();
+        } else if(sc.nextInt() == 4) {
+            DoctorLogin doc = new DoctorLogin();
+            doc.run();
+        }
         Action action = new Action(); // Initialize com.centrailized_medi_application.Action
         Patient p1 = new Patient();  // Initialize patient
         PatientLogin p_login = new PatientLogin(p1);  // Passing the object to the patient login
@@ -17,7 +33,6 @@ public class TestAction
         // Setting com.centrailized_medi_application.Patient id and pass manually for now
 //        p_login.setPatient_name("Rag1234345"); // com.centrailized_medi_application.Patient id
 //        p_login.setPatient_pass("qwerty"); // com.centrailized_medi_application.Patient pass
-
 
         p_login.setPatient_name("jaggi");
         p_login.setPatient_pass("2");
