68cef3463ecd9866921f326a5f30696bc073eee9
Aditya Jain <ad487131@dal.ca>
2021-07-09 00:45:59 +0530

    Remove MainMenu & Menu

--- src/main/java/com/centrailized_medi_application/Menu.java
+++
@@ -1,9 +0,0 @@
-package com.centrailized_medi_application;
-
-import java.io.IOException;
-import java.sql.SQLException;
-
-public interface Menu
-{
-    void display() throws SQLException, IOException, ClassNotFoundException;
-}
