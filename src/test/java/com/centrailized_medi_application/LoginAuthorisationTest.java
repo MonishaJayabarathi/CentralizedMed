f8dabb03e09826f2ae582119af07e4cde6d28599
Aditya Jain <ad487131@dal.ca>
2021-07-09 22:34:08 +0530

    TestCase getSecurityQuestion()

--- src/test/java/com/centrailized_medi_application/LoginAuthorisationTest.java
+++ src/test/java/com/centrailized_medi_application/LoginAuthorisationTest.java
@@ -1,7 +1,34 @@
 package com.centrailized_medi_application;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.lang.Math;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class LoginAuthorisationTest {
+class LoginAuthorisationTest
+{
+
+
+    @Test
+    void verifySecurityAnswer() throws SQLException, IOException, ClassNotFoundException {
+        boolean[] creds = new boolean[2];
+        String environment = "src/main/resources/config_test.properties";
+        DB_Connection db = new DB_Connection(environment);
+        Connection connect = db.createConnection();
+        String user_name = "Neelay@gmail.com";
+        PreparedStatement answer = connect.prepareStatement("select * from userinfo where emailId=?");
+        answer.setString(1,user_name);
+        ResultSet s1 = answer.executeQuery();
+        s1.next();
+        LoginAuthorisation key = new LoginAuthorisation();
+        assertEquals(s1.getString("firstAnswer"),key.getSecurityQuestion(),"Incorrect Security Answer");
+
+
+    }
 
 }
