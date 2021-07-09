4f1e5291a026ff735b676503218eecfa40625db6
Aditya Jain <ad487131@dal.ca>
2021-07-09 22:45:54 +0530

    Add functionality getSecurityAnswer

--- src/main/java/com/centrailized_medi_application/ILoginAuthorisation.java
+++ src/main/java/com/centrailized_medi_application/ILoginAuthorisation.java
@@ -1,10 +1,13 @@
 package com.centrailized_medi_application;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
 public interface ILoginAuthorisation {
 
     static int retry=0;
     String user_id = null;
-    public String getSecurityQuestion();//will be used to get the security questions for the user trying to login
+    public String getSecurityQuestion() throws SQLException, IOException, ClassNotFoundException;//will be used to get the security questions for the user trying to login
     public String resetPassword();//This function will provide the user the ability to reset password
 
 }
--- src/main/java/com/centrailized_medi_application/LoginAuthorisation.java
+++ src/main/java/com/centrailized_medi_application/LoginAuthorisation.java
@@ -1,9 +1,23 @@
 package com.centrailized_medi_application;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class LoginAuthorisation implements ILoginAuthorisation {
     @Override
-    public String getSecurityQuestion() {
-        return null;
+    public String getSecurityQuestion() throws SQLException, IOException, ClassNotFoundException {
+        String environment = "src/main/resources/config_test.properties";
+        DB_Connection db = new DB_Connection(environment);
+        Connection connect = db.createConnection();
+        String user_name = "Neelay@gmail.com";
+        PreparedStatement answer = connect.prepareStatement("select * from userinfo where emailId=?");
+        answer.setString(1, user_name);
+        ResultSet s1 = answer.executeQuery();
+        s1.next();
+        return s1.getString("firstAnswer");
     }
 
     @Override
