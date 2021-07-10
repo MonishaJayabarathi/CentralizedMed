1088d7a41a4d2b6f74811734a05a49b797f5a525
Neelay Jayantbharti Goswami <nl339853@dal.ca>
2021-07-10 15:45:39 +0530

    Add Exception Handling in PatientRegistration

--- src/main/java/com/centrailized_medi_application/PatientRegistration.java
+++ src/main/java/com/centrailized_medi_application/PatientRegistration.java
@@ -7,6 +7,7 @@ import java.util.*;
 
 public class PatientRegistration {
     private static BasicDetails patientBasicDetails;
+    private static Validation patientValidation;
     // Mentioned all the variables names which will be used for
     //Storing values
     private String firstAnswer;
@@ -28,23 +29,23 @@ public class PatientRegistration {
         return firstAnswer;
     }
 
-    public void setFirstAnswer(String firstAnswer){ this.firstAnswer = firstAnswer; }
+    public void setFirstAnswer(String firstAnswer) {
+        this.firstAnswer = firstAnswer;
+    }
 
     public String getSecondAnswer() {
         return secondAnswer;
     }
 
-   public void setSecondAnswer(String secondAnswer)
-   {
-       this.secondAnswer = secondAnswer;
-   }
+    public void setSecondAnswer(String secondAnswer) {
+        this.secondAnswer = secondAnswer;
+    }
 
-    public String getThirdAnswer()
-    {
+    public String getThirdAnswer() {
         return thirdAnswer;
     }
 
-    public void setThirdAnswer(String thirdAnswer){
+    public void setThirdAnswer(String thirdAnswer) {
         this.thirdAnswer = thirdAnswer;
     }
 
@@ -74,44 +75,41 @@ public class PatientRegistration {
         this.chronicDisease = chronicDisease;
     }
 
-    public String getInsuranceNo()
-    {
-        return  insuranceNo;
+    public String getInsuranceNo() {
+        return insuranceNo;
     }
 
-    public void setInsuranceNo(String insuranceNo){
+    public void setInsuranceNo(String insuranceNo) {
         this.insuranceNo = insuranceNo;
     }
 
-    public String getDonorCardNo()
-    {
+    public String getDonorCardNo() {
         return donorCardNo;
     }
 
-    public void setDonorCardNo(String donorCardNo){
+    public void setDonorCardNo(String donorCardNo) {
         this.donorCardNo = donorCardNo;
     }
 
-    public String getFamilyMemberCode(){
+    public String getFamilyMemberCode() {
         return familyMemberCode;
     }
 
-    public void setFamilyMemberCode(String familyMemberCode){
+    public void setFamilyMemberCode(String familyMemberCode) {
         this.familyMemberCode = familyMemberCode;
     }
 
-    public String getVolunteer()
-    {
+    public String getVolunteer() {
         return volunteer;
     }
 
-    public void setVolunteer(String volunteer){
+    public void setVolunteer(String volunteer) {
         this.volunteer = volunteer;
     }
 
     //Method for providing interface to interact with the user in order to get values
 
-    public void beginRegistration(){
+    public void beginRegistration() throws ClassNotFoundException, SQLException, IOException {
 
         try (Scanner scanner = new Scanner(System.in)) {
             patientBasicDetails = new BasicDetails();
@@ -120,23 +118,11 @@ public class PatientRegistration {
             System.out.println();
             System.out.println("Please enter the below details in order to get registered as a patient successfully");
 
-//            boolean checker = false;
-//            do {
-//
-//            }while(checker) {
-//                //User Details
-//                System.out.print(" Enter firstName: ");
-//                patientBasicDetails.setFirstName(scanner.nextLine());
-//                if(!) {
-//                    checker = true;
-//                }
-//                else {
-//                    checker = false;
-//                }
-//            }
 
             System.out.print(" Enter FirstName: ");
-            patientBasicDetails.setFirstName(scanner.nextLine());
+            String nametemp = scanner.nextLine();
+            patientBasicDetails.setFirstName(nametemp);
+
 
             System.out.print(" Enter lastName: ");
             patientBasicDetails.setLastName(scanner.nextLine());
@@ -153,6 +139,8 @@ public class PatientRegistration {
             String contactNo = scanner.nextLine();
             patientBasicDetails.setContactNo(contactNo);
 
+
+
             System.out.print(" Enter address: ");
             String address = scanner.nextLine();
             patientBasicDetails.setAddress(address);
@@ -168,10 +156,9 @@ public class PatientRegistration {
             String confirmPassword = scanner.nextLine();
             if (confirmPassword.equals(password)) {
 
-                    patientBasicDetails.setPassword(confirmPassword);
+                patientBasicDetails.setPassword(confirmPassword);
 
-            }
-            else {
+            } else {
                 System.out.println("Password dont match");
 
             }
@@ -227,123 +214,80 @@ public class PatientRegistration {
 
             System.out.println("[firstName:" + patientBasicDetails.getFirstName() + ", lastName:" +
                     patientBasicDetails.getLastName() + ", gender:" + patientBasicDetails.getGender() + ", dateOfBirth:" +
-                    patientBasicDetails.getDob()+ ", contactNo:" + patientBasicDetails.getContactNo() +", address:" +patientBasicDetails.getAddress()+", password:"
-                    +patientBasicDetails.getPassword()+" ,emailId:"+patientBasicDetails.getEmailId()+ ", " +
-                    "firstAnswer:" +firstAnswer+", secondAnswer:"+secondAnswer+", thirdAnswer:"+
-                    thirdAnswer+", bloodGroup:"+bloodGroup+", allergies:"+allergy+", chronicDisease:"+
-                    chronicDisease+", insuranceNo:"+insuranceNo+ ", donarCardNo:"+donorCardNo+
-                    ", familyMemberCode:"+familyMemberCode+", volunteer:"+volunteer+"]");
+                    patientBasicDetails.getDob() + ", contactNo:" + patientBasicDetails.getContactNo() + ", address:" + patientBasicDetails.getAddress() + ", password:"
+                    + patientBasicDetails.getPassword() + " ,emailId:" + patientBasicDetails.getEmailId() + ", " +
+                    "firstAnswer:" + firstAnswer + ", secondAnswer:" + secondAnswer + ", thirdAnswer:" +
+                    thirdAnswer + ", bloodGroup:" + bloodGroup + ", allergies:" + allergy + ", chronicDisease:" +
+                    chronicDisease + ", insuranceNo:" + insuranceNo + ", donarCardNo:" + donorCardNo +
+                    ", familyMemberCode:" + familyMemberCode + ", volunteer:" + volunteer + "]");
 
             //taking user input for choice to insert value in the database
             int submit = scanner.nextInt();
 
             // Inserting values in the database if user finalize the information entered
-            if(submit == 1){
+            if (submit == 1) {
                 update();
-            }
-            else
-            {
+            } else {
                 System.out.println("You have decided not to submit your details so now you have to fill the form again");
                 //Asking user to enter details again if they wish to change any information
                 beginRegistration();
 
             }
-        }
-        //catch in order to handle any exceptions related to sql occurs
-        catch (SQLException throwables) {
-            throwables.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
+
 // update method to insert values in the database after user's choice is entered
 
-    public void update() throws ClassNotFoundException, SQLException, IOException
-    {
+    public void update() throws ClassNotFoundException, SQLException, IOException {
+        try {
             System.out.println("Inserting values in the database");
-                   //Setting up the connection with the database
-//                    String driver = "com.mysql.cj.jdbc.Driver";
-//                    String url = "jdbc:mysql://localhost:3306/trytest";
-//                    String uname = "root";
-//                    String pass = "";
-//                    Class.forName(driver);
+            //Setting up the connection with the database
+            DbConnection one = new DB_Connection("src/main/resources/config_test.properties");
+            Connection c = one.createConnection();
+            //prepare statment to insert values in the database
+            PreparedStatement test = c.prepareStatement("INSERT INTO `userinfo`(firstname,lastname," +
+                    "dateofbirth,gender,password,emailId,address,contactNo,firstAnswer,secondAnswer,thirdAnswer," +
+                    "bloodGroup,allergy,chronicDisease,insuranceNo,donorCardNo,familyMemberCode,volunteer)" +
+                    "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
-//                    Connection c = DriverManager.getConnection(url, uname, pass);
-                    DbConnection one = new DB_Connection("src/main/resources/config_test.properties");
-                    Connection c = one.createConnection();
-                    //prepare statment to insert values in the database
-                    PreparedStatement test = c.prepareStatement("INSERT INTO `userinfo`(firstname,lastname," +
-                            "dateofbirth,gender,password,emailId,address,contactNo,firstAnswer,secondAnswer,thirdAnswer," +
-                            "bloodGroup,allergy,chronicDisease,insuranceNo,donorCardNo,familyMemberCode,volunteer)" +
-                            "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            //inserting values one by one for each entities
 
-                    //inserting values one by one for each entities
+            test.setString(1, patientBasicDetails.getFirstName());
+            test.setString(2, patientBasicDetails.getLastName());
+            test.setString(3, patientBasicDetails.getDob());
+            test.setString(4, patientBasicDetails.getGender());
+            test.setString(5, patientBasicDetails.getPassword());
+            test.setString(6, patientBasicDetails.getEmailId());
+            test.setString(7, patientBasicDetails.getAddress());
+            test.setString(8, patientBasicDetails.getContactNo());
+            test.setString(9, firstAnswer);
+            test.setString(10, secondAnswer);
+            test.setString(11, thirdAnswer);
+            test.setString(12, bloodGroup);
+            test.setString(13, allergy);
+            test.setString(14, chronicDisease);
+            test.setString(15, insuranceNo);
+            test.setString(16, donorCardNo);
+            test.setString(17, familyMemberCode);
+            test.setString(18, volunteer);
 
-                    test.setString(1, patientBasicDetails.getFirstName());
-                    test.setString(2, patientBasicDetails.getLastName());
-                    test.setString(3, patientBasicDetails.getDob());
-                    test.setString(4, patientBasicDetails.getGender());
-                    test.setString(5, patientBasicDetails.getPassword());
-                    test.setString(6, patientBasicDetails.getEmailId());
-                    test.setString(7, patientBasicDetails.getAddress());
-                    test.setString(8, patientBasicDetails.getContactNo());
-                    test.setString(9, firstAnswer);
-                    test.setString(10, secondAnswer);
-                    test.setString(11, thirdAnswer);
-                    test.setString(12, bloodGroup);
-                    test.setString(13, allergy);
-                    test.setString(14, chronicDisease);
-                    test.setString(15, insuranceNo);
-                    test.setString(16, donorCardNo);
-                    test.setString(17, familyMemberCode);
-                    test.setString(18, volunteer);
+            //executing update
+            test.executeUpdate();
+            System.out.println(patientBasicDetails.getFirstName() + "  has successfully Registered");
+            System.out.println("Now you would be able to login main menu");
+            WelcomePage init = new WelcomePage();
+            init.display();
+        } catch (Exception e) {
 
-                    //executing update
-                    test.executeUpdate();
-                    System.out.println(patientBasicDetails.getFirstName()+"  has successfully Registered");
-                    System.out.println("Now you would be able to login main menu");
-                    WelcomePage init = new WelcomePage();
-                    init.display();
+            System.out.println("Exception Encountered Moving to Main Menu");
+            WelcomePage init = new WelcomePage();
+            init.display();
+        }
     }
-
-    public void get(String Email) throws SQLException, ClassNotFoundException, IOException {
-//                String driver = "com.mysql.cj.jdbc.Driver";
-//                String url = "jdbc:mysql://localhost:3306/trytest";
-//                String uname = "root";
-//                String pass = "";
-//                Class.forName(driver);
-                DbConnection two = new DB_Connection("src/main/resources/config_test.properties");
-                Connection c = two.createConnection();
-                this.em = Email;
-                System.out.println("Fetching value for the user whose EmailId:"+em);
-                String sqlStmt = "SELECT * FROM userinfo where emailId =?";
-                PreparedStatement prepStmt = c.prepareStatement(sqlStmt);
-                prepStmt.toString();
-                prepStmt.setString(1,em);
-                ResultSet rs = prepStmt.executeQuery();
-                while(rs.next()){
-                    //Display values
-                    System.out.println("Firstname: " + rs.getString("firstname"));
-                    System.out.println("Lastname: " + rs.getString("lastname"));
-                    System.out.println("Date of birth: " + rs.getString("dateofbirth"));
-                    System.out.println("Gender: " + rs.getString("gender"));
-                    System.out.println("EmailId: " + rs.getString("emailId"));
-                    System.out.println("Address: " + rs.getString("address"));
-                    System.out.println("BloodGroup: " + rs.getString("bloodGroup"));
-                    System.out.println("Allergy: " + rs.getString("allergy"));
-                    System.out.println("ChronicDisease: " + rs.getString("chronicDisease"));
-                    System.out.println("InsuranceNo: " + rs.getString("insuranceNo"));
-                    System.out.println("DonorCardNo: " + rs.getString("insuranceNo"));
-                    System.out.println("FamilyMemberCode: " + rs.getString("familyMemberCode"));
-                    System.out.println("Volunteer: " + rs.getString("volunteer"));
-                }
-
-    }
-
 }
-
 
 
 
