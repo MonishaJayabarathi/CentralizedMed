77ad5257b884961c9f66d83662ffbf7d256d93fa
Neelay Jayantbharti Goswami <nl339853@dal.ca>
2021-07-10 15:46:50 +0530

    Add Test class for About Patient

---
+++ src/test/java/com/centrailized_medi_application/AboutPatientTest.java
@@ -0,0 +1,12 @@
+package com.centrailized_medi_application;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AboutPatientTest {
+
+}
