2c922c2588076c28f6bd4ee6396c2f44fdb4e7f8
Monisha J <mn504849@dal.ca>
2021-07-12 00:54:35 +0530

    Refactored About Interface, and related About Doctor flow

--- src/main/java/com/centrailized_medi_application/AboutDoctor.java
+++ src/main/java/com/centrailized_medi_application/AboutDoctor.java
@@ -1,26 +1,34 @@
 package com.centrailized_medi_application;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
-/*
+/**
  * @author Monisha J
  * @description: This program implements About Interface.
- * To load and display details of a doctor.
+ * To fetch and display details of a doctor.
  * Overrides the methods defining structure of execution.
- * @params: int doctor_id : gets id of the doctor who logged in.
+ * @params: int doctor_id : gets username of the doctor who logged in.
+ * DoctorDashboard dd : this triggers return to users dashboard.
  */
-
 public class AboutDoctor implements About {
 
-  private int doctorId; //holds the give doctor id.
+  DoctorDashboard init;
+  private String doctorUsername; //holds the give doctor username.
+  private ResultSet currentDoctorDetails; // holds the db results fetched
 
-  AboutDoctor(int doctor_id) {
-    this.doctorId = doctor_id;
+  AboutDoctor(String doctor_id, DoctorDashboard dd) {
+    this.doctorUsername = doctor_id;
+    this.init = dd;
   }
 
+  //fetch user details from DB
   @Override
-  public void loadDetails() throws SQLException, IOException, ClassNotFoundException {
+  public void fetchDetails() throws SQLException, IOException, ClassNotFoundException {
     //get details from DB
     System.out.println("Loading About...");
   }
@@ -29,5 +37,10 @@ public class AboutDoctor implements About {
   public void displayDetails() throws SQLException, IOException, ClassNotFoundException {
     //display details
     System.out.println("Doctor Details");
+  }
+
+  @Override
+  public void back() throws SQLException, IOException, ClassNotFoundException {
+    //navigation to doctor dashboard
   }
 }
