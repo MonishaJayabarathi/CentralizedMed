986a65ed2901582622a01e6bdfb09155564504b8
Neelay Jayantbharti Goswami <nl339853@dal.ca>
2021-07-11 19:06:34 +0530

    Add New Test Case for PatientRegistrationTest

--- src/test/java/com/centrailized_medi_application/PatientRegistrationTest.java
+++ src/test/java/com/centrailized_medi_application/PatientRegistrationTest.java
@@ -1,47 +1,54 @@
-//package com.centrailized_medi_application;
-//
-//import org.junit.jupiter.api.Test;
-//import static org.junit.jupiter.api.Assertions.*;
-//public class PatientRegistrationTest {
-//
-//    //Creating test cases for the registration patient class for various set & get function
-//    @Test
-//    void RegistrationTest()
-//    {
-//        PatientRegistration testcheck = new PatientRegistration();
-//
-//        testcheck.setFirstAnswer("SGhighschool");
-//        assertEquals("SGhighschool",testcheck.getFirstAnswer());
-//
-//        testcheck.setSecondAnswer("Swim");
-//        assertEquals("Swim",testcheck.getSecondAnswer());
-//
-//        testcheck.setThirdAnswer("Honda");
-//        assertEquals("Honda",testcheck.getThirdAnswer());
-//
-//        testcheck.setBloodGroup("o+ve");
-//        assertEquals("o+ve",testcheck.getBloodGroup());
-//
-//        testcheck.setAllergy("pollen");
-//        assertEquals("pollen",testcheck.getAllergy());
-//
-//        testcheck.setChronicDisease("tonsilitis");
-//        assertEquals("tonsilitis",testcheck.getChonicDisease());
-//
-//        testcheck.setDonorCardNo("qwe123");
-//        assertEquals("qwe123",testcheck.getDonorCardNo());
-//
-//        testcheck.setFamilyMemberCode("fam1");
-//        assertEquals("fam1",testcheck.getFamilyMemberCode());
-//
-//        testcheck.setVolunteer("yes");
-//        assertEquals("yes",testcheck.getVolunteer());
-//
-//        testcheck.setInsuranceNo("insure1");
-//        assertEquals("insure1",testcheck.getInsuranceNo());
-//
-//    }
-//
-//
-//
-//}
+package com.centrailized_medi_application;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class PatientRegistrationTest {
+    BasicDetails tempbasic = new BasicDetails();
+    PatientDetails tempdetails = new PatientDetails();
+    SecurityQuestions tempsecurity = new SecurityQuestions();
+
+    @Test
+    void RegistrationTest()
+    {
+
+        tempbasic.setFirstName("one");
+        tempbasic.setLastName("two");
+        tempbasic.setDob("06/07/1999");
+        tempbasic.setGender("Male");
+        tempbasic.setPassword("test@ONE23");
+        tempbasic.setEmailId("two@gmail.com");
+        tempbasic.setAddress("Sector-29 Gandhinagar");
+        tempbasic.setContactNo("4234234232");
+        tempdetails.setBloodGroup("o+ve");
+        tempdetails.setAllergy("pollengrains");
+        tempdetails.setChronicDisease("tonsilitis");
+        tempdetails.setInsuranceNo("OI9900909");
+        tempdetails.setDonorCardNo("SDFS00322");
+        tempdetails.setFamilyMemberCode("FSDFE0000");
+        tempdetails.setVolunteer("yes");
+
+        tempbasic.setLatitude(100);
+        tempbasic.setLongitude(200);
+
+        tempsecurity.setAnswer1("SG");
+        tempsecurity.setAnswer2("SWIM");
+        tempsecurity.setAnswer3("Honda");
+    }
+    @Disabled
+    @Test
+    @DisplayName("To execute doctor registration")
+    void execute() throws SQLException, IOException, ClassNotFoundException {
+        RegistrationTest();
+        NewPatient newone = new NewPatient(tempbasic,tempdetails,tempsecurity, new WelcomePage());
+        PatientRegistration patienttest = new PatientRegistration(newone,new WelcomePage());
+        assertEquals(true,newone.getRegistrationStatus(), "Already registered patient execution " +
+                "should return false");
+    }
+}
+
