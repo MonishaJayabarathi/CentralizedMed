fb00cf6d549733830ac1d4b414b68516ce96c13c
Aditya Jain <ad487131@dal.ca>
2021-07-11 06:37:01 +0530

    Add functionality link_patient() in AddPatient

--- src/main/java/com/centrailized_medi_application/AddPatient.java
+++ src/main/java/com/centrailized_medi_application/AddPatient.java
@@ -1,7 +1,12 @@
 package com.centrailized_medi_application;
 
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class AddPatient {
 
@@ -9,9 +14,11 @@ public class AddPatient {
   private String p_name = null;
   private boolean[] creds = new boolean[2];
   DbConnection db_access;
+  private String docter_user_name = null;
 
-  public AddPatient(DbConnection connection) {
+  public AddPatient(DbConnection connection, String doc_name) {
     db_access = connection;
+    docter_user_name = doc_name;
   }
 
   // Verifies weather person exists in system
@@ -27,8 +34,23 @@ public class AddPatient {
     }
   }
 
-  public String link_patient(String user_name)
-  {
-    return null;
+  public String link_patient() throws SQLException {
+    AddPatient new_entry = new AddPatient(db_access, docter_user_name);
+    Connection connection = db_access.createConnection();
+    DateTimeFormatter consultation_date = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+    LocalDateTime current_time = LocalDateTime.now();
+    if (new_entry.verify_patient(p_name)) {
+      PreparedStatement prep_statement = connection.prepareStatement("INSERT INTO `consultations`(doctor_username,patient_username,consultation_date) VALUES (?, ?, ?)");
+      prep_statement.setString(1, docter_user_name);
+      prep_statement.setString(2, p_name);
+      prep_statement.setString(3, (consultation_date.format(current_time)));
+      prep_statement.executeUpdate();;
+      return "Added Successfully!";
+    }
+    else
+    {
+      System.out.println("Error");
+      return "Patient is not registered into the system!";
+    }
   }
 }
--- src/test/java/com/centrailized_medi_application/AddPatientTest.java
+++ src/test/java/com/centrailized_medi_application/AddPatientTest.java
@@ -17,9 +17,10 @@ class AddPatientTest {
   @Test
   public void fetch_details_and_add() throws SQLException, IOException, ClassNotFoundException {
     boolean exist = true;
+    String doc_name = "key@gmail.com";
     String patient_name = "Aditya@hotmail.com";
     DB_Connection db_access = new DB_Connection(environment,patient_name,null);
-    AddPatient new_entry = new AddPatient(db_access);
+    AddPatient new_entry = new AddPatient(db_access,doc_name);
     assertEquals(exist, new_entry.verify_patient(patient_name), "Patient is not registered into the system");
 
   }
@@ -27,9 +28,10 @@ class AddPatientTest {
   @Test
   public void fetch_details_and_add_invalid() throws SQLException, IOException, ClassNotFoundException {
     boolean exist = false;
+    String doc_name = "key@gmail.com";
     String patient_name = "Nihal@hotmail.com";
     DB_Connection db_access = new DB_Connection(environment,patient_name,null);
-    AddPatient new_entry = new AddPatient(db_access);
+    AddPatient new_entry = new AddPatient(db_access,doc_name);
     assertEquals(exist, new_entry.verify_patient(patient_name), "Patient is registered into the system");
   }
 
@@ -37,10 +39,11 @@ class AddPatientTest {
   public void link_patient_with_doc() throws SQLException, IOException, ClassNotFoundException { String docter_name = "key@gmail.com";
     String patient_name = "Aditya@hotmail.com";
     DB_Connection db_access = new DB_Connection(environment,patient_name,null);
-    AddPatient new_entry = new AddPatient(db_access);
+    String doc_name = "key@gmail.com";
+    AddPatient new_entry = new AddPatient(db_access,doc_name);
     if(new_entry.verify_patient(patient_name))
     {
-      assertEquals("Added successfully!", new_entry.link_patient(patient_name),"Could not add patient");
+      assertEquals("Added Successfully!", new_entry.link_patient(),"Could not add patient");
     }
 
   }
