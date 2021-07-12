edc9b4229d5695a74036a446eb7c678db998c17c
Kazi Hasan <kz970073@dal.ca>
2021-07-13 04:54:28 +0600

    Add requirements and empty method for setLatLon, getLatLon, SetSpecialization

--- src/main/java/com/centrailized_medi_application/PatientSuggestions.java
+++ src/main/java/com/centrailized_medi_application/PatientSuggestions.java
@@ -1,16 +1,37 @@
 package com.centrailized_medi_application;
 
+import java.util.Scanner;
+
 public class PatientSuggestions {
     private String userName;
     private float latitude;
     private float longitude;
     private String suggestedDoctors;
+    private String specialization;
 
-    /* given patient username, retrieve latitude longitude for the patient
+    /* initialize the class using patient username */
+    public PatientSuggestions(String userName){
+        this.userName = userName;
+
+    }
+
+    /* use patient username to retrieve latitude longitude for the patient
      * connect to patient's information table
-     * use patient username to retrieve latitude longitude */
-    public PatientSuggestions(String patientUserName){
+     * use patient username to retrieve latitude longitude
+     */
+    public void setLatLon(){
 
+    }
+
+    /* get latitude and longitude of the patient */
+    public void getLatLon(){
+
+    }
+
+    /* ask patient input for specialization */
+    public void setSpecialization() {
+        //Scanner inputSpecialization = new Scanner(System.in);
+        //this.specialization = inputSpecialization.nextLine();
     }
 
     /* for current patient retrieve top rated specialized doctors within 10km
@@ -21,13 +42,13 @@ public class PatientSuggestions {
      */
     public void retrieveDoctorSuggestions(){
 
-
     }
 
     /* getter function for list of suggested doctors */
     public String getSuggestedDoctors(){
         return suggestedDoctors;
     }
+
 
 
 }
---
+++ src/test/java/com/centrailized_medi_application/PatientSuggestionsTest.java
@@ -0,0 +1,17 @@
+package com.centrailized_medi_application;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PatientSuggestionsTest {
+
+
+    @Test
+    void retrieveDoctorSuggestions() {
+    }
+
+    @Test
+    void getSuggestedDoctors() {
+    }
+}
