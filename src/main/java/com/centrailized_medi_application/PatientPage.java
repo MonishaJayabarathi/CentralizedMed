e0755c5d09f09b462b35aafd3045a72bcbd31878
Neelay Jayantbharti Goswami <nl339853@dal.ca>
2021-07-13 03:23:32 +0530

    Update PatientPage with one constructor to take username and update About method which helps in getting information about user

--- src/main/java/com/centrailized_medi_application/PatientPage.java
+++ src/main/java/com/centrailized_medi_application/PatientPage.java
@@ -1,10 +1,25 @@
 package com.centrailized_medi_application;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
 public class PatientPage extends PatientDashboard
 {
     private int patient_id;
+
+    protected String patientUsername; //gives id of logged in as patient
+
+    PatientPage(String username) {
+        this.patientUsername = username;
+    }
+
+
+
     @Override
-    public void About() {
+    public void About() throws SQLException, IOException, ClassNotFoundException {
+        AboutPatient aboutPatient = new AboutPatient(this.patientUsername, this); //testing commit
+        AboutPatientPage aboutPatientpage = new AboutPatientPage(aboutPatient);
+        aboutPatientpage.display();
 
     }
 
