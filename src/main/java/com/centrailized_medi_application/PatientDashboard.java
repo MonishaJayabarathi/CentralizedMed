041b0453b5a2fafb53b78d3e79978940aa657bd1
Neelay Jayantbharti Goswami <nl339853@dal.ca>
2021-07-13 22:30:02 +0530

    Update from Develop applied

--- src/main/java/com/centrailized_medi_application/PatientDashboard.java
+++ src/main/java/com/centrailized_medi_application/PatientDashboard.java
@@ -7,12 +7,16 @@ import java.util.Scanner;
 public abstract class PatientDashboard extends Dashboard{
     public abstract void About() throws SQLException, IOException, ClassNotFoundException;
 
-    public abstract void Consultations();
+    public abstract void Consultations() throws SQLException, IOException, ClassNotFoundException;
 
-    public abstract void Prescriptions();
+    public abstract void Prescriptions() throws SQLException, IOException, ClassNotFoundException;
 
-    public abstract void Suggestions();
+    public abstract void Suggestions() throws SQLException, IOException, ClassNotFoundException;
+
+    public abstract void Logout() throws SQLException, IOException, ClassNotFoundException;
+
     protected boolean flag = false;
+    protected boolean logout=false;
     protected Scanner sc = new Scanner(System.in);
     @Override
     public void display()throws SQLException, IOException, ClassNotFoundException{
@@ -21,6 +25,7 @@ public abstract class PatientDashboard extends Dashboard{
         System.out.println("2.Consultations");
         System.out.println("3.Prescriptions");
         System.out.println("4.Suggestions");
+        System.out.println("5.Logout");
         System.out.println("----------------------------------");
         System.out.println(" Enter from above options to proceed:");
 
@@ -28,18 +33,22 @@ public abstract class PatientDashboard extends Dashboard{
             Scanner sc = new Scanner(System.in);
             int option = sc.nextInt();
             if (option == 1) {
-
-
-                this.About();  // navigates to display About Doctor Tab.
+                this.About();
                 flag = true;
             } else if (option == 2) {
+                this.Consultations();
                 flag = true;
             } else if (option == 3) {
                 this.Prescriptions();
                 flag = true;
             } else if (option == 4) {
                 flag = true;
-            } else {
+            }else if(option==5) {
+                this.Logout();
+                logout=true;
+                flag=true;
+            }
+            else {
                 System.out.println("Enter the correct options to proceed");
             }
         }
