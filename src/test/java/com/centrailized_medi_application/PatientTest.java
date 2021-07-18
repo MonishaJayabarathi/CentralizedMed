8dd8406fe833a3f73a6d5a63e58400006ee0ebbe
Ridampreet Jaggi <rd285404@dal.ca>
2021-07-18 19:18:24 +0530

    Fixing code written in wrong file

--- src/test/java/com/centrailized_medi_application/PatientTest.java
+++ src/test/java/com/centrailized_medi_application/PatientTest.java
@@ -3,7 +3,6 @@ package com.centrailized_medi_application;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import javax.print.Doc;
 import java.io.IOException;
 import java.sql.SQLException;
 
@@ -82,10 +81,13 @@ class PatientTest {
     void authenticate_pass_invalid() throws SQLException, IOException, ClassNotFoundException {
         String user_name = "test";
         String password= "a12";
-        Doctor doctor = new Doctor(new WelcomePage(),new DoctorPage(user_name));
-        DoctorLogin d_login = new DoctorLogin(doctor, new WelcomePage());
-        d_login.setDoctorName(user_name);
-        d_login.setDoctorPassword(password);
-
+        Patient patient = new Patient(new WelcomePage(),new PatientPage(user_name),new DB_Connection(environment,user_name,password));
+        PatientLogin p_login = new PatientLogin(patient, new WelcomePage());
+        p_login.setPatient_name(user_name);
+        p_login.setPatient_pass(password);
+        patient.fetch(p_login.getPatient_name(),p_login.getPatient_pass());
+        patient.validate();
+        patient.authenticate();
+        assertTrue(patient.get_auth_status());
     }
 }
