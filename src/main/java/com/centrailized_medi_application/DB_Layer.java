fb9c9a5d0dfbc785f3e908a3e8d6771b1041c29a
Ridampreet Jaggi <rd285404@dal.ca>
2021-07-20 04:37:56 +0530

    DB layer implementation for Add Patient

--- src/main/java/com/centrailized_medi_application/DB_Layer.java
+++ src/main/java/com/centrailized_medi_application/DB_Layer.java
@@ -5,9 +5,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class DB_Layer {
-
+String environment="src/main/resources/config_test.properties";
   public ResultSet getUserDetails(String username,String user_type) throws ClassNotFoundException, IOException, SQLException {
 
     String sqlStmt;
@@ -28,6 +30,17 @@ public class DB_Layer {
     currentDoctorDetails = prepStmt.executeQuery();
 
     return currentDoctorDetails;
+  }
+
+  public void insertConsultations(String docter_user_name, String p_name, DateTimeFormatter consultation_date, LocalDateTime current_time) throws SQLException, IOException, ClassNotFoundException {
+      DB_Connection db_access=new DB_Connection(environment);
+      Connection local = db_access.createConnection();
+      PreparedStatement prep_statement = local.prepareStatement("INSERT INTO `consultations`(doctor_username,patient_username,consultation_date_and_time) VALUES (?, ?, ?)");
+      prep_statement.setString(1, docter_user_name);
+      prep_statement.setString(2, p_name);
+      prep_statement.setString(3, (consultation_date.format(current_time)));
+      prep_statement.executeUpdate();
+
   }
 
 
