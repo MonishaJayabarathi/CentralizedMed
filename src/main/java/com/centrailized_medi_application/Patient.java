b26887d00c84367f0fd6315956357306a523c0a6
Aditya Jain <ad487131@dal.ca>
2021-07-19 19:35:28 +0530

    Refactor Patient.java and Modify WelcomePage

--- src/main/java/com/centrailized_medi_application/Patient.java
+++ src/main/java/com/centrailized_medi_application/Patient.java
@@ -5,16 +5,16 @@ import java.sql.SQLException;
 
 /* Implementation for com.centrailized_medi_application.Patient */
 public class Patient extends Login {
-    private String user_name; // Stores Patient name
+    private String userName; // Stores Patient name
     private String password; // Stores patient passwd
     private boolean validId = false; // Sets true when patient is registered
     private boolean validPsswd = false; // Sets true when patient is registered
     private boolean authenticatePhase = false;
-    private boolean[] creds = new boolean[2];
-    private MainDashboard init;
-    private PatientDashboard pd;
+    private boolean[] credentials = new boolean[2];
+    private MainDashboard dashboard;
+    private PatientDashboard patientDashboard;
     private DbConnection connect;
-    private ILoginAuthorisation la;
+    private ILoginAuthorisation loginAuthorisation;
     private static int localRetry=0;
 
 
@@ -23,7 +23,7 @@ public class Patient extends Login {
     }
 
     public String getUsername() {
-        return this.user_name;
+        return this.userName;
     }
 
     public boolean get_id_status() {
@@ -39,26 +39,28 @@ public class Patient extends Login {
     }
 
 
-    public Patient(MainDashboard init, PatientDashboard patient_menu, DbConnection connection, ILoginAuthorisation loginauth) {
-        this.init = init;
-        this.pd = patient_menu;
+    public Patient(MainDashboard dashboard, PatientDashboard patient_menu, DbConnection connection, ILoginAuthorisation loginauth) {
+        this.dashboard = dashboard;
+        this.patientDashboard = patient_menu;
         this.connect = connection;
-        this.la = loginauth;
+        this.loginAuthorisation = loginauth;
     }
 
     /* Fetching patient credentials*/
     @Override
-    public void fetch(String u_name, String psswd) {
-        this.user_name = u_name;
+    public void fetch(String userName, String psswd) {
+        this.userName = userName;
         this.password = psswd;
     }
+
+
 
     /* Connect to database and check against the data*/
     @Override
     public void validate() throws SQLException, IOException, ClassNotFoundException {
-        creds = connect.getDetails();
-        this.validId = creds[0];
-        this.validPsswd = creds[1];
+        credentials = connect.getDetails();
+        this.validId = credentials[0];
+        this.validPsswd = credentials[1];
     }
 
     /* Defining the placeholder for com.centrailized_medi_application.Patient authentication*/
@@ -68,22 +70,22 @@ public class Patient extends Login {
         authenticatePhase = true;
         if (this.validId && this.validPsswd) // Exist and valid credentials
         {
-            System.out.println("Welcome " + this.user_name); // name to be replaced with the actual name stored in db
-            this.pd.display();
+            System.out.println("Welcome " + this.userName); // name to be replaced with the actual name stored in db
+            this.patientDashboard.display();
         } else if (this.validId && !this.validPsswd) {
             //call authentication mechanism
             System.out.println("Check your credentials!");
             localRetry++;
-            this.la.set_Retry(localRetry);
+            this.loginAuthorisation.set_Retry(localRetry);
             if(localRetry!=3) {
-                this.init.display_patient_login();
+                this.dashboard.display_patient_login();
             }else if(localRetry==3){
-                this.la.getSecurityQuestion(user_name);
+                this.loginAuthorisation.getSecurityQuestion(userName);
             }
         } else {
             System.out.println("Please register to the system!");
             System.out.println("Navigating to main menu...");
-            init.display();
+            dashboard.display();
         }
 
     }
--- src/main/java/com/centrailized_medi_application/WelcomePage.java
+++ src/main/java/com/centrailized_medi_application/WelcomePage.java
@@ -14,7 +14,7 @@ public class WelcomePage extends MainDashboard
         System.out.println("Enter your password:");
         String patient_pass = (sc.next());
         Action action = new Action(); // Initialize Action
-        Patient p1 = new Patient(this,new PatientPage(patient_name), new DB_Connection(environment,patient_name,patient_pass));  // Initialize patient
+        Patient p1 = new Patient(this,new PatientPage(patient_name), new DB_Connection(environment,patient_name,patient_pass),new LoginAuthorisation());  // Initialize patient
         PatientLogin p_login = new PatientLogin(p1, this);  // Passing the object to the patient login
         p_login.setPatientName(patient_name);
         p_login.setPatientPass(patient_pass);
@@ -25,15 +25,15 @@ public class WelcomePage extends MainDashboard
     @Override
     public void display_patient_registration() throws SQLException, IOException, ClassNotFoundException {
         try {
-            BasicDetails basicDetails = new BasicDetails();
-            PatientDetails patientDetails = new PatientDetails();
-            SecurityQuestions securityQuestions = new SecurityQuestions();
-            Action action = new Action(); // Initialize Action
-            NewPatient p = new NewPatient(basicDetails, patientDetails, securityQuestions, this );
-            PatientRegistration patientReg = new PatientRegistration(p,this);
-            patientReg.start();
-            action.setCommand(patientReg);
-            action.run();
+//            BasicDetails basicDetails = new BasicDetails();
+//            PatientDetails patientDetails = new PatientDetails();
+//            SecurityQuestions securityQuestions = new SecurityQuestions();
+//            Action action = new Action(); // Initialize Action
+//            NewPatient p = new NewPatient(basicDetails, patientDetails, securityQuestions, this );
+//            PatientRegistration patientReg = new PatientRegistration(p,this);
+//            patientReg.start();
+//            action.setCommand(patientReg);
+//            action.run();
 
         }
         catch (Exception e)
@@ -47,30 +47,30 @@ public class WelcomePage extends MainDashboard
     @Override
     public void display_doctor_registration() throws SQLException, IOException, ClassNotFoundException {
 
-        BasicDetails basicDetails = new BasicDetails();
-        DoctorDetails doctorDetails = new DoctorDetails();
-        SecurityQuestions securityQuestions = new SecurityQuestions();
-        Action action = new Action(); // Initialize Action
-        NewDoctor doc = new NewDoctor(basicDetails, doctorDetails, securityQuestions, this );
-        DoctorRegistration docReg = new DoctorRegistration(doc, this);
-        docReg.start();
-        action.setCommand(docReg);
-        action.run();
+//        BasicDetails basicDetails = new BasicDetails();
+//        DoctorDetails doctorDetails = new DoctorDetails();
+//        SecurityQuestions securityQuestions = new SecurityQuestions();
+//        Action action = new Action(); // Initialize Action
+//        NewDoctor doc = new NewDoctor(basicDetails, doctorDetails, securityQuestions, this );
+//        DoctorRegistration docReg = new DoctorRegistration(doc, this);
+//        docReg.start();
+//        action.setCommand(docReg);
+//        action.run();
 
     }
     @Override
     public void display_doctor_login() throws SQLException, IOException, ClassNotFoundException {
-        System.out.println("Enter your username:");
-        String doctor_name = (sc.next());
-        System.out.println("Enter your password:");
-        String doctor_password = (sc.next());
-        Action action = new Action(); // Initialize Action
-        Doctor dr = new Doctor(this,new DoctorPage(doctor_name));  // Initialize doctor
-        DoctorLogin doctorLogin = new DoctorLogin(dr, this);  // Passing the object to the doctor login
-        doctorLogin.setDoctorName(doctor_name);
-        doctorLogin.setDoctorPassword(doctor_password);
-        action.setCommand(doctorLogin);
-        action.run();
+//        System.out.println("Enter your username:");
+//        String doctor_name = (sc.next());
+//        System.out.println("Enter your password:");
+//        String doctor_password = (sc.next());
+//        Action action = new Action(); // Initialize Action
+//        Doctor dr = new Doctor(this,new DoctorPage(doctor_name));  // Initialize doctor
+//        DoctorLogin doctorLogin = new DoctorLogin(dr, this);  // Passing the object to the doctor login
+//        doctorLogin.setDoctorName(doctor_name);
+//        doctorLogin.setDoctorPassword(doctor_password);
+//        action.setCommand(doctorLogin);
+//        action.run();
 
     }
 
