7054311edcdb85f9282f809b5e7708353c713db1
Ridampreet Jaggi <rd285404@dal.ca>
2021-07-20 14:09:56 +0530

    Layer refactoring PatientSuggestions class

--- src/main/java/com/centrailized_medi_application/DB_Layer.java
+++ src/main/java/com/centrailized_medi_application/DB_Layer.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class DB_Layer {
 String environment="src/main/resources/config_test.properties";
@@ -221,5 +222,91 @@ Connection connection=db.createConnection();
     insert_statement.execute();
     connection.close();
   }
+
+
+  public void insertNewPatient(BasicDetails basicDetails,PatientDetails patientDetails,SecurityQuestions securityQuestions) throws SQLException, IOException, ClassNotFoundException {
+    DB_Connection db=new DB_Connection("src/main/resources/config_test.properties");
+    Connection connection=db.createConnection();
+
+    PreparedStatement st =connection.prepareStatement("Insert into login_details(user_name,pass) values(?,?)");
+    st.setString(1,basicDetails.getEmailId());
+    st.setString(2,basicDetails.getPassword());
+
+    st.execute();
+    st = connection.prepareStatement("Select * from login_details where user_name=\"" + basicDetails.getEmailId() + "\"");
+    ResultSet rs2 = st.executeQuery();
+    rs2.next();
+    int curr_id = rs2.getInt("idlogin_details");
+
+    PreparedStatement test=connection.prepareStatement("INSERT INTO patient_info(id,firstname,lastname," +
+        "dateOfbirth,gender,password,emailId,address,contactNo,bloodGroup," +
+        "allergy,chronicDisease,insuranceNo,donorCardNo,familyMemberCode,volunteer,security_answer_1,security_answer_2,security_answer_3,latitude,longitude) " +
+        "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+    test.setInt(1,curr_id);
+    test.setString(2,basicDetails.getFirstName());
+    test.setString(3,basicDetails.getLastName());
+    test.setString(4,basicDetails.getDob());
+    test.setString(5,basicDetails.getGender());
+    test.setString(6,basicDetails.getPassword());
+    test.setString(7,basicDetails.getEmailId());
+    test.setString(8,basicDetails.getAddress());
+    test.setString(9,basicDetails.getContactNo());
+    test.setString(10,patientDetails.getBloodGroup());
+    test.setString(11,patientDetails.getAllergy());
+    test.setString(12,patientDetails.getChonicDisease());
+    test.setString(13,patientDetails.getInsuranceNo());
+    test.setString(14,patientDetails.getDonorCardNo());
+    test.setString(15,patientDetails.getFamilyMemberCode());
+    test.setString(16,patientDetails.getVolunteer());
+    test.setString(17,securityQuestions.getAnswer1());
+    test.setString(18,securityQuestions.getAnswer2());
+    test.setString(19,securityQuestions.getAnswer3());
+    test.setInt(20,basicDetails.getLatitude());
+    test.setInt(21,basicDetails.getLongitude());
+    test.execute();
+  }
+
+
+  public void feedRatings(String userName) throws SQLException, IOException, ClassNotFoundException {
+    DbConnection one = new DB_Connection(environment);
+    Connection connection = one.createConnection();
+
+    PreparedStatement prep_statement = connection.prepareStatement("SELECT * FROM `consultations` WHERE patient_username = ?");
+    prep_statement.setString(1, userName);
+    ResultSet doc_list = prep_statement.executeQuery();
+    //prep_statement.close();
+    Scanner sc = new Scanner(System.in);
+    while (doc_list.next()){
+      System.out.println(doc_list.getString("doctor_username") + " " + doc_list.getString("consultation_date_and_time"));
+      System.out.println("Enter rating:");
+      int rating = sc.nextInt();
+      int current_value = 0;
+
+      //Fetch existing values
+      String email_doc =doc_list.getString("doctor_username");
+      PreparedStatement current_val = connection.prepareStatement("SELECT * FROM `doctor_info` WHERE emailId=?");
+      current_val.setString(1, email_doc);
+      //current_val.setInt(2, rating);
+      ResultSet get_current_value = current_val.executeQuery();
+      int updt_rating = 0;
+      if(get_current_value.next()) {
+        current_value = get_current_value.getInt("rating");
+        updt_rating = (current_value + rating) / 2;
+      }
+      else
+      {
+        updt_rating = rating;
+      }
+
+      //Update with the patient values
+      PreparedStatement pstatement = connection.prepareStatement("Update doctor_info set rating=? where emailId=?");
+      pstatement.setInt(1, updt_rating);
+      email_doc =doc_list.getString("doctor_username");
+      pstatement.setString(2, email_doc);
+      boolean doc_rate = pstatement.execute();
+    }
+  }
+
 
 }
