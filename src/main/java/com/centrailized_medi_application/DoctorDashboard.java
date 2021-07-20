ff1b18d2bfe80a64e8c4267ee16f4536316de30b
Monisha J <mn504849@dal.ca>
2021-07-21 00:22:51 +0530

    Added Past Consultations tab test cases and defined appropriate methods satisfying the same

--- src/main/java/com/centrailized_medi_application/DoctorDashboard.java
+++ src/main/java/com/centrailized_medi_application/DoctorDashboard.java
@@ -6,8 +6,10 @@ import java.util.Scanner;
 
 /**
  * @author Monisha J
- * @description: This program receives input for navigation
+ * @description : This program receives input for navigation
  * inside a Doctor Dashboard and routes the pages accordingly
+ * It holds the abstract methods that are to be defined to show
+ * details specific to the tab selected.
  */
 
 public abstract class DoctorDashboard extends Dashboard {
@@ -19,13 +21,18 @@ public abstract class DoctorDashboard extends Dashboard {
 
 
   public abstract void display_donors() throws SQLException, IOException, ClassNotFoundException;
-  public abstract void display_patients() throws SQLException, IOException, ClassNotFoundException;
-  public abstract void display_patiient_family_history() throws SQLException, IOException, ClassNotFoundException;
-  public abstract void Logout() throws SQLException, IOException, ClassNotFoundException;
-  protected boolean flag = false; //
-  protected boolean logout=false;
 
-  /* Method the displays the available tabs and corresponding key
+  public abstract void display_pastConsultations() throws SQLException, IOException, ClassNotFoundException;
+
+  public abstract void display_patient_family_history() throws SQLException, IOException, ClassNotFoundException;
+
+  public abstract void Logout() throws SQLException, IOException, ClassNotFoundException;
+
+  protected boolean flag = false;
+  protected boolean logout = false;
+
+  /**
+   * Method the displays the available tabs and corresponding keys
    * Reads integer option from console and reroutes accordingly
    */
   @Override
@@ -33,14 +40,14 @@ public abstract class DoctorDashboard extends Dashboard {
     System.out.println("-----------Dashboard-------------");
     System.out.println("1.About");
     System.out.println("2.Add Patients");
-    System.out.println("3.Patients");
+    System.out.println("3.Past Consultations");
     System.out.println("4.Donors");
     System.out.println("5.Patient Family History");
     System.out.println("6.Logout");
     System.out.println("----------------------------------");
     System.out.println(" Enter from above options to proceed:");
 
-    while (!flag) {
+    while (flag != true) {
       Scanner sc = new Scanner(System.in);
       int option = sc.nextInt();
       if (option == 1) {
@@ -50,20 +57,19 @@ public abstract class DoctorDashboard extends Dashboard {
         this.display_add_patients();
         flag = true;
       } else if (option == 3) {
+        this.display_pastConsultations();
         flag = true;
-      } else if(option==4) {
+      } else if (option == 4) {
         this.display_donors();
-        flag=true;
-      }else if(option==5) {
-        this.display_patiient_family_history();
-        flag=true;
-      }
-      else if(option==6) {
+        flag = true;
+      } else if (option == 5) {
+        this.display_patient_family_history();
+        flag = true;
+      } else if (option == 6) {
         this.Logout();
-        logout=true;
-        flag=true;
-      }
-      else {
+        logout = true;
+        flag = true;
+      } else {
         System.out.println("Enter the correct options to proceed");
       }
     }
