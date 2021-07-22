f75286489c53ec04d50f3ed6208b7ae3a14db3bd
Aditya Jain <ad487131@dal.ca>
2021-07-23 03:51:09 +0530

    Refactor variable names and add comments

--- src/main/java/com/centrailized_medi_application/AddPatient.java
+++ src/main/java/com/centrailized_medi_application/AddPatient.java
@@ -7,25 +7,45 @@ import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * @author Aditya Jain & Ridampreet Singh
+ * @description: This class implements the the feature of linking a patient with doctor's name.
+ * Whenever a Patient visits the doctor, the doctor will add a patient (through his portal), which will record the
+ * date and time of the consultation. The will later help the patient to rate the doctor as per their
+ * experience.
+ */
+
 public class AddPatient {
   private boolean patientExist = false;
   private boolean[] credentials = new boolean[2];
   private boolean registered = false;
   private String patientUserName = null;
-  private String doctorUserName = null;
+  private String doctorUserName;
   DbConnection dbAccess;
+  DB_Layer layer = new DB_Layer();
 
-  public AddPatient(DbConnection connection, String doc_name) {
+  /**
+   * Constructor with DbConnection and String as input parameters
+   *
+   * @Param connection as DbConnection Interface
+   * @Param docName as doctor's username
+   */
+  public AddPatient(DbConnection connection, String docName) throws SQLException, IOException, ClassNotFoundException {
     dbAccess = connection;
-    doctorUserName = doc_name;
+    doctorUserName = docName;
   }
 
-  // Verifies weather person exists in system
-  public boolean verify_patient(String patient_name) throws SQLException, IOException, ClassNotFoundException {
-    patientUserName = patient_name;
-    credentials = dbAccess.getDetails();
+  /**
+   * This method checks whether a Patient is registered to the system or not.
+   * Because only registered patient can be linked under doctor's profile.
+   *
+   * @return boolean
+   * @Param patientName
+   */
+  boolean patientPresence(String patientName) throws SQLException, IOException, ClassNotFoundException {
+    patientUserName = patientName;
+    credentials = layer.getCredStatus(patientUserName,null);
 
-    //Checking only against username ( index 0 is for username )
     if (credentials[0] != false) {
       this.patientExist = true;
       return patientExist;
@@ -34,11 +54,17 @@ public class AddPatient {
     }
   }
 
+  /**
+   * This method checks whether a Patient is registered to the system or not.
+   * Because only registered patient can be linked under doctor's profile.
+   *
+   * @return boolean
+   * @Param patientName
+   */
   public boolean link_patient(String patient_name) throws SQLException, IOException, ClassNotFoundException {
     DateTimeFormatter consultation_date = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
     LocalDateTime current_time = LocalDateTime.now();
-    if (this.verify_patient(patient_name)) {
-      DB_Layer layer = new DB_Layer();
+    if (this.patientPresence(patient_name)) {
       layer.insertConsultations(doctorUserName, patientUserName, consultation_date, current_time);
       System.out.println("Added Successfully!");
       registered = true;
