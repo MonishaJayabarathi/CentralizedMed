0ed5abdbd1c0cab6fb19d1335caba537d771a0af
Ridampreet Jaggi <rd285404@dal.ca>
2021-07-26 02:13:54 +0530

    removed throws

--- src/main/java/com/centrailized_medi_application/AboutDoctor.java
+++ src/main/java/com/centrailized_medi_application/AboutDoctor.java
@@ -23,7 +23,7 @@ public class AboutDoctor implements About {
   private ResultSet currentDoctorDetails; // holds the db results fetched
   private PreparedStatement prepStmt;
   DB_Layer layer;
-  AboutDoctor(String doctor_id, DoctorDashboard dd) throws SQLException, IOException, ClassNotFoundException {
+  AboutDoctor(String doctor_id, DoctorDashboard dd)  {
     this.doctorUsername = doctor_id;
     this.init = dd;
     //this.layer=DB_Layer.singleConnection();
@@ -31,51 +31,66 @@ public class AboutDoctor implements About {
 
   //fetch user details from DB
   @Override
-  public void fetchDetails() throws SQLException, IOException, ClassNotFoundException {
+  public void fetchDetails() {
+    try{
     this.layer=DB_Layer.singleConnection();
     System.out.println("Loading About...\n");
     List<Object> resultState =layer.getUserDetails(doctorUsername,"Doctor");//call to the DB layer.
 
     this.currentDoctorDetails = (ResultSet) resultState.get(0);
     this.prepStmt = (PreparedStatement) resultState.get(1);
+    }catch (Exception e){
+      System.out.println(e.getMessage());
+    }
 
   }
   //display user details
   @Override
-  public void displayDetails() throws SQLException {
-    while (this.currentDoctorDetails.next()) {
-      System.out.println("**************************** About You **************************");
-      System.out.println("Username(E-mail): " + this.currentDoctorDetails.getString("emailId"));
-      System.out.println("Id: " + this.currentDoctorDetails.getString("id"));
-      System.out.println("Firstname: " + this.currentDoctorDetails.getString("firstname"));
-      System.out.println("Lastname: " + this.currentDoctorDetails.getString("lastname"));
-      System.out.println("Gender: " + this.currentDoctorDetails.getString("gender"));
-      System.out.println("Date of birth: " + this.currentDoctorDetails.getString("dateOfBirth"));
-      System.out.println("Registration Number: " + this.currentDoctorDetails.getString("registrationNumber"));
-      System.out.println("Specilaity: " + this.currentDoctorDetails.getString("speciality"));
-      System.out.println("Address: " + this.currentDoctorDetails.getString("address"));
-      System.out.println("Contact: " + this.currentDoctorDetails.getString("contactNo"));
-      System.out.println("*****************************************************************");
-    }
+  public void displayDetails()  {
+    try {
+      while (this.currentDoctorDetails.next()) {
+        System.out.println("**************************** About You **************************");
+        System.out.println("Username(E-mail): " + this.currentDoctorDetails.getString("emailId"));
+        System.out.println("Id: " + this.currentDoctorDetails.getString("id"));
+        System.out.println("Firstname: " + this.currentDoctorDetails.getString("firstname"));
+        System.out.println("Lastname: " + this.currentDoctorDetails.getString("lastname"));
+        System.out.println("Gender: " + this.currentDoctorDetails.getString("gender"));
+        System.out.println("Date of birth: " + this.currentDoctorDetails.getString("dateOfBirth"));
+        System.out.println("Registration Number: " + this.currentDoctorDetails.getString("registrationNumber"));
+        System.out.println("Specilaity: " + this.currentDoctorDetails.getString("speciality"));
+        System.out.println("Address: " + this.currentDoctorDetails.getString("address"));
+        System.out.println("Contact: " + this.currentDoctorDetails.getString("contactNo"));
+        System.out.println("*****************************************************************");
+      }
 
-    currentDoctorDetails.close();
-    prepStmt.close();
-    layer.close();
+      currentDoctorDetails.close();
+      prepStmt.close();
+      layer.close();
+    }
+    catch (SQLException s){
+      System.out.println(s.getMessage());
+    }
   }
 
   //handles navigation to dashboard
   @Override
-  public void back  () throws SQLException, IOException, ClassNotFoundException {
-    System.out.println("Press 1 to move to your Dashboard");
-    Scanner sc = new Scanner(System.in);
-    int option = sc.nextInt();
-    if (option == 1) {
-      System.out.println("Returning to your Dashboard...");
-      WelcomePage init = new WelcomePage();
-      this.init.display();
-    } else {
-      System.out.println("Please provide a valid entry");
-      this.back();
+  public void back  () {
+
+    try {
+      System.out.println("Press 1 to move to your Dashboard");
+      Scanner sc = new Scanner(System.in);
+      int option = sc.nextInt();
+      if (option == 1) {
+        System.out.println("Returning to your Dashboard...");
+        WelcomePage init = new WelcomePage();
+        this.init.display();
+      } else {
+        System.out.println("Please provide a valid entry");
+        this.back();
+      }
+    }
+    catch(Exception e){
+      System.out.println(e.getMessage());
     }
   }
 }
