fcb3d82a2a961f6babb5b642391ec4134a42f08c
Aditya Jain <ad487131@dal.ca>
2021-07-28 06:08:52 +0530

    Add missing comment

--- src/main/java/com/centrailized_medi_application/Registration.java
+++ src/main/java/com/centrailized_medi_application/Registration.java
@@ -1,6 +1,6 @@
 package com.centrailized_medi_application;
 
-/**
+/**@author Monisha J
  * This interface declares the methods to be defined while a user registration is carried out.
  */
 public interface Registration {
